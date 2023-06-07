ae41c8cec4f3f8901f968d17ac4b8a7bde647cff
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-07 15:09:22 +0900

    Add manufacturer controller skeleton code

---
+++ controllers/manufacturerController.js
@@ -0,0 +1,31 @@
+exports.manufacturer_list = (req, res, next) => {
+  res.send("Not implemented yet. manufacturer LIST");
+};
+
+exports.manufacturer_create_get = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer CREATE FORM: ${req.params.id}`);
+};
+
+exports.manufacturer_create_post = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer CREATE POST: ${req.params.id}`);
+};
+
+exports.manufacturer_update_get = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer UPDATE FORM: ${req.params.id}`);
+};
+
+exports.manufacturer_update_post = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer UPDATE POST: ${req.param.id}`);
+};
+
+exports.manufacturer_delete_get = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer DELETE FORM: ${req.params.id}`);
+};
+
+exports.manufacturer_delete_post = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer DELETE POST: ${req.params.id}`);
+};
+
+exports.manufacturer_detail = (req, res, next) => {
+  res.send(`Not implemented yet. manufacturer DETAILS: ${req.params.id}`);
+};
--- routes/manufacturers.js
+++ routes/manufacturers.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+const manufacturer_controller = require("../controllers/manufacturerController");
+
 /// MANUFACTURER ROUTES ///
 
 // GET manufacturers
-router.get("/", (req, res, next) => {});
+router.get("/", manufacturer_controller.manufacturer_list);
 
 // GET request for creating manufacturer
-router.get("/:id/create", (req, res, next) => {});
+router.get("/:id/create", manufacturer_controller.manufacturer_create_get);
 
 // POST request for creating manufacturer
-router.post("/:id/create", (req, res, next) => {});
+router.post("/:id/create", manufacturer_controller.manufacturer_create_post);
 
 // GET request for updating manufacturer
-router.get("/:id/update", (req, res, next) => {});
+router.get("/:id/update", manufacturer_controller.manufacturer_update_get);
 
 // POST request for updating manufacturer
-router.post("/:id/update", (req, res, next) => {});
+router.post("/:id/update", manufacturer_controller.manufacturer_update_post);
 
 // GET request for deleting manufacturer
-router.get("/:id/delete", (req, res, next) => {});
+router.get("/:id/delete", manufacturer_controller.manufacturer_delete_get);
 
 // POST request for deleting manufacturer
-router.post("/:id/delete", (req, res, next) => {});
+router.post("/:id/delete", manufacturer_controller.manufacturer_delete_post);
 
 // GET request for a manufacturer
-router.get("/:id", (req, res, next) => {});
+router.get("/:id", manufacturer_controller.manufacturer_detail);
 
 module.exports = router;
