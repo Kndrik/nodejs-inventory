7dddb18561123e54f817028d4d9b465185bd4d54
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-07 16:22:38 +0900

    Add express async handler and complete index page

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,5 +1,20 @@
-exports.display_inventory = (req, res, next) => {
+const Category = require("../models/category");
+const Item = require("../models/item");
+const Manufacturer = require("../models/manufacturer");
+
+const asyncHandler = require("express-async-handler");
+
+exports.display_inventory = asyncHandler(async (req, res, next) => {
+  const [numCategories, numItems, numManufacturers] = await Promise.all([
+    Category.countDocuments().exec(),
+    Item.countDocuments().exec(),
+    Manufacturer.countDocuments().exec(),
+  ]);
+
   res.render("index", {
-    title: "Inventory",
+    title: "Index",
+    category_count: numCategories,
+    item_count: numItems,
+    manufacturer_count: numManufacturers,
   });
-};
+});
