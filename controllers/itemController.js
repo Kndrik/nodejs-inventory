845110fd01125026f229f209f889e623ac163a06
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-07 15:11:26 +0900

    Add item controller skeleton code

---
+++ controllers/itemController.js
@@ -0,0 +1,31 @@
+exports.item_list = (req, res, next) => {
+  res.send("Not implemented yet. item LIST");
+};
+
+exports.item_create_get = (req, res, next) => {
+  res.send(`Not implemented yet. item CREATE FORM: ${req.params.id}`);
+};
+
+exports.item_create_post = (req, res, next) => {
+  res.send(`Not implemented yet. item CREATE POST: ${req.params.id}`);
+};
+
+exports.item_update_get = (req, res, next) => {
+  res.send(`Not implemented yet. item UPDATE FORM: ${req.params.id}`);
+};
+
+exports.item_update_post = (req, res, next) => {
+  res.send(`Not implemented yet. item UPDATE POST: ${req.param.id}`);
+};
+
+exports.item_delete_get = (req, res, next) => {
+  res.send(`Not implemented yet. item DELETE FORM: ${req.params.id}`);
+};
+
+exports.item_delete_post = (req, res, next) => {
+  res.send(`Not implemented yet. item DELETE POST: ${req.params.id}`);
+};
+
+exports.item_detail = (req, res, next) => {
+  res.send(`Not implemented yet. item DETAILS: ${req.params.id}`);
+};
--- routes/items.js
+++ routes/items.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+const item_controller = require("../controllers/itemController");
+
 /// ITEM ROUTES ///
 
 // GET items
-router.get("/", (req, res, next) => {});
+router.get("/", item_controller.item_list);
 
 // GET request for creating item
-router.get("/:id/create", (req, res, next) => {});
+router.get("/:id/create", item_controller.item_create_get);
 
 // POST request for creating item
-router.post("/:id/create", (req, res, next) => {});
+router.post("/:id/create", item_controller.item_create_post);
 
 // GET request for updating item
-router.get("/:id/update", (req, res, next) => {});
+router.get("/:id/update", item_controller.item_update_get);
 
 // POST request for updating item
-router.post("/:id/update", (req, res, next) => {});
+router.post("/:id/update", item_controller.item_update_post);
 
 // GET request for deleting item
-router.get("/:id/delete", (req, res, next) => {});
+router.get("/:id/delete", item_controller.item_delete_get);
 
 // POST request for deleting item
-router.post("/:id/delete", (req, res, next) => {});
+router.post("/:id/delete", item_controller.item_delete_post);
 
 // GET request for a item
-router.get("/:id", (req, res, next) => {});
+router.get("/:id", item_controller.item_detail);
 
 module.exports = router;
