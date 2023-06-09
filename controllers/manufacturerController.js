b2d050abb45a0bf0e7b74f155ff4735761a0c9a2
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-09 14:28:51 +0900

    Add manufacturer update

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -66,13 +66,64 @@ exports.manufacturer_create_post = [
   }),
 ];
 
-exports.manufacturer_update_get = (req, res, next) => {
-  res.send(`Not implemented yet. manufacturer UPDATE FORM: ${req.params.id}`);
-};
+exports.manufacturer_update_get = asyncHandler(async (req, res, next) => {
+  const manufacturer = await Manufacturer.findById(req.params.id).exec();
 
-exports.manufacturer_update_post = (req, res, next) => {
-  res.send(`Not implemented yet. manufacturer UPDATE POST: ${req.param.id}`);
-};
+  res.render("manufacturer_form", {
+    title: "Update Manufacturer",
+    manufacturer: manufacturer,
+  });
+});
+
+exports.manufacturer_update_post = [
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("address", "Address must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const manufacturer = new Manufacturer({
+      name: req.body.name,
+      address: req.body.address,
+      _id: req.params.id,
+    });
+
+    req.newManufacturer = manufacturer;
+
+    if (!errors.isEmpty()) {
+      res.render("manufacturer_form", {
+        title: "New manufacturer",
+        manufacturer: manufacturer,
+        errors: errors.array(),
+      });
+    } else {
+      next();
+    }
+  }),
+
+  asyncHandler(async (req, res, next) => {
+    // Check if manufacturer already exists
+    const existingManufacturer = await Manufacturer.findOne({
+      name: req.body.name,
+    }).exec();
+
+    if (
+      existingManufacturer !== null &&
+      existingManufacturer._id.toString() !== req.params.id.toString()
+    ) {
+      res.render("manufacturer_form", {
+        title: "New manufacturer",
+        manufacturer: req.newManufacturer,
+        errors: [{ msg: "A manufacturer with this name already exists." }],
+      });
+    } else {
+      await Manufacturer.findByIdAndUpdate(req.params.id, req.newManufacturer);
+      res.redirect(req.newManufacturer.url);
+    }
+  }),
+];
 
 exports.manufacturer_delete_get = (req, res, next) => {
   res.send(`Not implemented yet. manufacturer DELETE FORM: ${req.params.id}`);
