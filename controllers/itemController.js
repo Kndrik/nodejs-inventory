d851642941cbd1b32e4c52ef40131b5ec55c2473
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-09 14:40:48 +0900

    Add item update

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -37,7 +37,7 @@ exports.item_create_post = [
     .trim()
     .isLength({ min: 1 })
     .escape(),
-  body("price", "The price must be greater than 0.").isInt({ min: 0 }).escape(),
+  body("price").escape(),
   body("in_stock", "The number in stock must be greater than 0.")
     .isInt({
       min: 0,
@@ -81,13 +81,71 @@ exports.item_create_post = [
   }),
 ];
 
-exports.item_update_get = (req, res, next) => {
-  res.send(`Not implemented yet. item UPDATE FORM: ${req.params.id}`);
-};
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  const [item, allCategories, allManufacturers] = await Promise.all([
+    Item.findById(req.params.id).exec(),
+    Category.find({}, "name").exec(),
+    Manufacturer.find({}, "name").exec(),
+  ]);
 
-exports.item_update_post = (req, res, next) => {
-  res.send(`Not implemented yet. item UPDATE POST: ${req.param.id}`);
-};
+  res.render("item_form", {
+    title: "Update item",
+    item: item,
+    category_list: allCategories,
+    manufacturer_list: allManufacturers,
+  });
+});
+
+exports.item_update_post = [
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("description", "Must contain a description.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price").escape(),
+  body("in_stock", "The number in stock must be greater than 0.")
+    .isInt({
+      min: 0,
+    })
+    .escape(),
+  body("category").escape(),
+  body("manufacturer").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      number_in_stock: req.body.in_stock,
+      category: req.body.category,
+      manufacturer: req.body.manufacturer,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors
+
+      // Get categories and manufacturers to re render the form
+      const [allCategories, allManufacturers] = await Promise.all([
+        Category.find({}, "name").exec(),
+        Manufacturer.find({}, "name").exec(),
+      ]);
+
+      res.render("item_form", {
+        title: "New Item",
+        category_list: allCategories,
+        manufacturer_list: allManufacturers,
+        item: item,
+        errors: errors.array(),
+      });
+    } else {
+      await Item.findByIdAndUpdate(req.params.id, item);
+      res.redirect(item.url);
+    }
+  }),
+];
 
 exports.item_delete_get = (req, res, next) => {
   res.send(`Not implemented yet. item DELETE FORM: ${req.params.id}`);
